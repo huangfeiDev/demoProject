62bcce39dbd07918669ea183bb76fb3e39a8fee3
huangfeiDev <huangfeijunjun@sina.com>
2019-09-20 10:34:27 +0800

    master 提交

---
+++ src/main/java/com/demoprojec/DemoProjectApplication.java
@@ -0,0 +1,17 @@
+package com.demoprojec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangfei
+ * @createTime 2019/9/20 10:30
+ * @description
+ */
+@SpringBootApplication
+public class DemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProjectApplication.class,args);
+    }
+}
